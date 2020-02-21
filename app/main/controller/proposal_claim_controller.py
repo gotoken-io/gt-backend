009eabcfaa1e5a2efd1696f3c2c48f6b81df761b
leec <includeleec@gmail.com>
2020-02-22 02:11:26 +0800

    add cancel_claim_proposal, get_user_claims api.

--- app/main/controller/proposal_claim_controller.py
+++ app/main/controller/proposal_claim_controller.py
@@ -5,7 +5,7 @@ from app.main.util.decorator import admin_token_required, token_required
 from app.main.service.user_service import get_a_user_by_auth_token
 
 from app.main.util.dto.proposal_claim_dto import proposal_claim, api
-from app.main.service.proposal_claim_service import claim_proposal
+from app.main.service.proposal_claim_service import claim_proposal, cancel_claim_proposal, get_user_claims
 
 api_proposal_claim = api
 
@@ -27,3 +27,27 @@ class ProposalClaimAPI(Resource):
 
         if user:
             return claim_proposal(data=post_data, user_id=user.id)
+
+    @api_proposal_claim.doc('cancel claim a proposal')
+    @token_required
+    def put(self):
+        # get the post data
+        post_data = request.json
+        # get auth token
+        auth_token = request.headers.get('Authorization')
+        user = get_a_user_by_auth_token(auth_token)
+
+        if user:
+            return cancel_claim_proposal(data=post_data, user_id=user.id)
+
+
+@api_proposal_claim.route('/user/<user_id>')
+@api_proposal_claim.param('user_id', 'user id')
+class UserProposalClaimAPI(Resource):
+    """
+        User Proposals Claim Resource
+    """
+    @api_proposal_claim.doc('get a user claim proposals')
+    @api_proposal_claim.marshal_list_with(proposal_claim, envelope='data')
+    def get(self, user_id):
+        return get_user_claims(user_id=user_id)
--- app/main/service/proposal_claim_service.py
+++ app/main/service/proposal_claim_service.py
@@ -77,3 +77,72 @@ def claim_proposal(data, user_id):
         }
     }
     return response_object, 201
+
+
+# cancel claim proposal
+def cancel_claim_proposal(data, user_id):
+    # check post data.proposal_id required
+    proposal_id = data.get('proposal_id')
+    if not proposal_id:
+        response_object = {
+            'status': 'fail',
+            'message': 'proposal_id is required.',
+        }
+        return response_object, 200
+
+    # check proposal
+    proposal = Proposal.query.filter_by(id=proposal_id).first()
+    if not proposal:
+        response_object = {
+            'status': 'fail',
+            'message': 'proposal is not exists.',
+        }
+        return response_object, 200
+
+    # check user
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        response_object = {
+            'status': 'fail',
+            'message': 'user is not exists.',
+        }
+        return response_object, 200
+
+    try:
+        proposal_claim = ProposalClaim.query.filter_by(
+            proposal_id=proposal_id, user_id=user_id).first()
+        if not proposal_claim:
+            response_object = {
+                'status': 'fail',
+                'message': 'proposal claim record is not exists.',
+            }
+            return response_object, 200
+
+        proposal_claim.status = ProposalClaimStatus['cancel'].value
+        db.session.commit()
+
+        response_object = {
+            'status': 'success',
+            'message': 'proposal claim cancel success.',
+        }
+        return response_object, 200
+
+    except Exception as e:
+        print(e)
+        response_object = {'status': 'fail', 'message': str(e)}
+        return response_object, 401
+
+
+def get_user_claims(user_id):
+
+    # check user
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        response_object = {
+            'status': 'fail',
+            'message': 'user is not exists.',
+        }
+        return response_object, 200
+
+    user_claims = ProposalClaim.query.filter_by(user_id=user_id).all()
+    return user_claims
